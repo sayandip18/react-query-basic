09268998343418fe09fbfcde5dca0d8988b8d775
Sayandip Halder <sayandiph4@gmail.com>
2022-07-28 12:00:02 +0530

    basic routes and api setup

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RQSuperHeroesPage = () => {
+  return (
+    <div>RQSuperHeroesPage</div>
+  )
+}
+
+export default RQSuperHeroesPage
---
+++ src/components/SuperHeroes.page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuperHeroesPage = () => {
+  return (
+    <div>SuperHeroesPage</div>
+  )
+}
+
+export default SuperHeroesPage
