24ce2d95585674b5d1ed5a7b4a10736fb2752532
Sayandip Halder <sayandiph4@gmail.com>
2022-07-28 12:09:44 +0530

    render heroes from api

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const SuperHeroesPage = () => {
-  return (
-    <div>SuperHeroesPage</div>
-  )
+    const [isLoading, setIsLoading] = useState(true)
+    const [data, setData] = useState([])
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/superheroes')
+        .then(res => {
+            setData(res.data)
+            setIsLoading(false)
+        })
+        .catch(error => {
+            setError(error.message)
+            setIsLoading(false)
+        })
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    if (error) {
+    return <h2>{error}</h2>
+    }
+
+    return (
+        <div>
+            <h2>SuperHeroesPage</h2>
+            {
+                data.map(hero => (
+                    <div key={hero.name}>{hero.name}</div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default SuperHeroesPage
